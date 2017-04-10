58119bff84e829ab9a8fd5e58b50ae8cf0680096
Nathaniel Sigafoos <nathaniel.b.sigafoos@gmail.com>
2017-04-10 14:49:07 -0400

    Added convenience scripts

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#This script compiles this java project
+#Used for convenience
+javac ./src/assignmentfive/*.java -d ./build/classes
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# This scriptexecutes this java project
+# Used for convenience
+java -cp "build/classes" assignmentfive.AssignmentFive
